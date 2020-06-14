9c359084537df644e70df5f6654c02d4872c1b3f
Anirudh Rath <cyberhybird@hotmail.com>
2020-06-14 08:26:50 +0530

    Developmental Changes  - 1
    Added Clause > Script can only be run as root 
    Added Persistence > Scripts settles and downloads essential files to run at start-up

--- updater.sh
+++ updater.sh
@@ -3,7 +3,24 @@ GREEN='\033[1;32m'
 NOCOLOR='\033[0m'
 TITLE='\033[0;33m'
 GIT='\033[1;35m'
-echo "Auto-Update"
+echo "Auto-Updater"
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "${TITLE}Preliminary Check Failed: ${RED}PLEASE RUN AS ROOT ${NOCOLOR}"
+  exit
+fi
+echo -e "${TITLE}Preliminary Check: ${GREEN}Successful${NOCOLOR}"
+echo -e "${TITLE}Phase Zero: ${GREEN}Setting-Up Auto Updater ${NOCOLOR}"
+cd /usr/local/sbin/
+echo Setting up script to run with Startup
+wget https://www.anirudhrath.tech/hostFiles/updater.sh
+
+echo Setting up updater script as a service
+cd /etc/systemd/system/
+wget https://www.anirudhrath.tech/hostFiles/updater.service
+
+echo Enabling updater.service
+sudo systemctl enable updater.service
 
 echo -e "${TITLE}Phase One: ${GREEN}Pre-Configuring Packages${NOCOLOR}"
 sudo dpkg --configure -a
@@ -16,27 +33,27 @@ sudo apt-get install -f
 echo
 
 echo -e "${TITLE}Phase Three: ${GREEN}Update Cache${NOCOLOR}"
-sudo apt update
+sudo apt update -y
 
 echo
 
 echo -e "${TITLE}Phase Four: ${GREEN}Upgrade Packages${NOCOLOR}"
-sudo apt upgrade
+sudo apt upgrade -y
 
 echo
 
 echo -e "${TITLE}Phase Five: ${GREEN}Upgrade Distro${NOCOLOR}"
-sudo apt dist-upgrade
+sudo apt dist-upgrade -y
 
 echo
 
 echo -e "${TITLE}Phase Six: ${GREEN}Uninstall Unused Packages${NOCOLOR}"
-sudo apt --purge autoremove
+sudo apt --purge autoremove -y
 
 echo
 
 echo -e "${TITLE}Phase Seven: ${GREEN}Clean Up${NOCOLOR}"
-sudo apt autoclean
+sudo apt autoclean -y
 
 echo
 
